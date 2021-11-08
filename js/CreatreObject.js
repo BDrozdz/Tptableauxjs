0f5ddf129fde1d2fd42ece99de32b486bb109534
Maxarsene <bruno.drozdz@live.fr>
2021-11-08 16:07:44 +0100

    CreateObject_exercice4

---
+++ js/CreatreObject.js
@@ -0,0 +1,10 @@
+let Maxarsene = {
+    //nom, age et mail sont des propriétés de l'objet "Maxarsene"
+    prenom : ['Bruno'],
+    nom : ['DROZDZ'],
+    age : 48,
+    classe : ['DW16'],
+    notes : [20,20,20,20,20,20]
+}
+
+console.log(Maxarsene);
