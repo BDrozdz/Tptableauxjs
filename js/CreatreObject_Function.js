5eab24da08917458467a504327dca23a77e19f45
Maxarsene <bruno.drozdz@live.fr>
2021-11-09 10:48:52 +0100

    correction_bug_chargement_array

--- js/CreatreObject_Function.js
+++ js/CreatreObject_Function.js
@@ -1,17 +1,25 @@
-let Etudiant = [['Bruno','DROZDZ',48,'DW16', [20,20,20,20,20,20]], 
+/*let Etudiant = [['Bruno','DROZDZ',48,'DW16', [20,20,20,20,20,20]], 
     ['Mètis','DROZDZ',24,'DW14', [15,15,15,15,15,15]], 
-    ['Esthète','DROZDZ',12,'DW12', [10,10,10,10,10,10]]];
+    ['Esthète','DROZDZ',12,'DW12', [10,10,10,10,10,10]]];*/
+
+let Etudiant = [];
+Etudiant[0] = ['Bruno','DROZDZ',48,'DW16', [20,20,20,20,20,20]];
+Etudiant[1] = ['Mètis','DROZDZ',24,'DW14', [15,15,15,15,15,15]];
+Etudiant[2] = ['Esthète','DROZDZ',12,'DW12', [10,10,10,10,10,10]];
+
 let EtudiantCreate = [];
 
 //EtudiantCreate = CreateObject(Etudiant); 
 
 Etudiant.forEach(element => { 
       
-    EtudiantCreate.push = CreateObject(element);
+    EtudiantCreate[EtudiantCreate.length]=element;
 
 });
 
-console.log(EtudiantCreate);
+EtudiantCreate.forEach(element => console.log(element));
+
+
 
 function CreateObject(Eleve){
     let eleve ={
